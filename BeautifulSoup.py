d1394ae0b35052a6d521fe09f27a4ec5a79fc4b1
MetsxxFan01 <102885372+MetsxxFan01@users.noreply.github.com>
2022-04-27 15:53:34 -0700

    Update BeautifulSoup.py

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,18 +1,21 @@
-
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
+import requests
+
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter URL: ')
-num = input('Enter count: ')
-pos = input('Enter position: ')
-print('Retrieving: ', url)
-for times in range(int(num)):
+num = int(input('Enter count: '))
+pos = int(input('Enter position: '))
+for tag in tags:
     html = urllib.request.urlopen(url, context=ctx).read()
+    load_the_webpage_content = requests.get("http://py4e-data.dr-chuck.net/known_by_Fikret.html")
     soup = BeautifulSoup(html, 'html.parser')
     tags = soup('a')
-    print('Retrieving: ', tags[int(pos)-1].get('href', None))
-    url = tags[int(pos)-1].get('href', None)
+while_loop = 5
+while while_loop > 0:
+    while_loop = while_loop -1
+print('Retrieving: ', soup)

1ffd173784e6ad0f5344dcb01fb745f471e239e4
MetsxxFan01 <102885372+MetsxxFan01@users.noreply.github.com>
2022-04-27 17:42:56 -0700

    Update BeautifulSoup.py

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -7,16 +7,27 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter URL: ')
-num = int(input('Enter count: '))
-pos = int(input('Enter position: '))
-html = urllib.request.urlopen(url, context=ctx).read()
-load_the_webpage_content = requests.get(url)
-soup = BeautifulSoup(html, 'html.parser')
-tags = soup.find_all('li')
-for tag in soup.find_all('li'):
-    print('%-10s : %s' % (tag.string, tag.a['href']))
-    while_loop = 5
-while while_loop > 0:
-    while_loop = while_loop -1
-    print('Retrieving: ', tag)
+url = input('Enter - ')
+count_number = int(input('Enter count: '))
+position_number = int(input('Enter position: ')) -1
+count1 = 0
+"""http://py4e-data.dr-chuck.net/known_by_Fikret.html"""
+
+def parse_website(url, position_number):     
+    url = tag[position_number ].get('href')
+    i = 0
+    for tag in tags:
+        i += 1
+        if i == position_number:
+            return tag.get('href', None)
+
+while count_number < count1:
+    print('Retrieving: ', url)
+    enter_website = parse_website(url, position_number)
+    count1 = count_number + 1
+    tags = soup('a')
+    name = tags[position_number].contents[0]
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html,'html.parser')
+
+print(name)

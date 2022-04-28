b4beb41735bf7b816288cd19ce29fc6eb9b25aa5
MetsxxFan01 <102885372+MetsxxFan01@users.noreply.github.com>
2022-04-27 20:37:52 -0700

    Update BeautifulSoup.py

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,3 +1,4 @@
+
 import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
@@ -12,14 +13,11 @@ count = int(input('Enter count: '))
 position = int(input('Enter position: '))
 
 load_the_webpage_content = requests.get(url)
-
-for tag in tags(range(count-1):
+for i in range(count):
+    i += 1
     html = urllib.request.urlopen(url, context=ctx).read()
     soup = BeautifulSoup(html, 'html.parser')
-    tags = soup.find_all('a')
-    url = tags[position - 1].a['href']
-    
-    while_loop = 5
-while while_loop > 0:
-    while_loop = while_loop -1
-print('Retrieving: ', url)
+    tags = soup('a')
+    newtag = tags[position-1]
+    url = newtag.get('href', None)
+    print(url)

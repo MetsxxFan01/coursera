9bc333dc6b976a1415e9050491fc8b5d68c0f0d4
MetsxxFan01 <102885372+MetsxxFan01@users.noreply.github.com>
2022-05-02 02:21:39 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -7,3 +7,10 @@ try:
     print("data has been successfully retrieved")
 except json.decoder.JSONDecodeError:
     print("There was a problem accessing the equipment data")
+    
+    #import json
+
+#with open("equipment.json") as file:
+	#data = json.load(file)
+
+#print("Equipment data has been successfully retrieved.") #comment the code above and than uncomment it to have the code above work

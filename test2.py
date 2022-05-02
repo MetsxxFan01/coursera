afe934eb2d65ec4327cecc850a11bb18c7f282dc
MetsxxFan01 <102885372+MetsxxFan01@users.noreply.github.com>
2022-05-02 02:37:15 -0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,16 +1,17 @@
-#import json
-
-#try:
-    #with (open("log.txt")) as file:
-        #data = json.load(file)
-    
-    #print("data has been successfully retrieved")
-#except json.decoder.JSONDecodeError:
-    #print("There was a problem accessing the equipment data")
-    
 import json
 
-with open("log.txt") as file:
-	data = json.load(file)
+try:
+    with (open("log.txt")) as file:
+        data = json.load(file)
+    
+    print("data has been successfully retrieved")
+except json.decoder.JSONDecodeError:
+    print("There was a problem accessing the equipment data")
+    
+#import json
 
-print("Equipment data has been successfully retrieved.") #comment the code above and than uncomment it to have the code above work
+#with open("log.txt") as file:
+	#data = json.load(file)
+
+#print("Equipment data has been successfully retrieved.") #comment the code above and than uncomment it to have the code above work
+#this code below doesn't work 
